04c9a0b4bff65c17c9a83cd070f37c5cffd30b6e
LOPESWI3 <104600109+LOPESWI3@users.noreply.github.com>
2022-10-13 13:22:40 -0700

    firebase sign-up config

--- tie-in-frontend/src/App.js
+++ tie-in-frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Login from './pages/Login';
+import SingUp from './pages/SignUp';
+import { AuthProvider } from './contexts/AuthContext';
 function App() {
   return (
+    <AuthProvider>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <Login/>
+        <br/> */}
+        <SingUp/>
       </header>
     </div>
+    </AuthProvider>
   );
 }
 
