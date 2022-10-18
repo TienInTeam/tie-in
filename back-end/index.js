c07538eb0d9d0ff2e80c25db7ed5ab1d3bec1da9
rojintag <106949675+rojintag@users.noreply.github.com>
2022-10-18 11:27:58 -0700

    feat: Add InputType Component

---
+++ back-end/index.js
@@ -0,0 +1,96 @@
+//Connect mongoDB
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://rojintg:mongo@newclaster.bmbuxcx.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+
+
+
+//Use Express
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`it is listening to ${PORT}`);
+})
+
+//Create endpoints
+
+//Read data
+app.get('/student', (req, res) => {
+
+  client.connect(async err => {
+    const collection = client.db("tiein").collection("student");
+    const query = { name: "Rojin" };
+    const student = await collection.findOne(query);
+    client.close();
+
+    res.status(200).send(student);
+  });
+
+
+});
+
+//Create data
+app.post('/student', (req, res) => {
+
+  const body = req.body
+
+  client.connect(async err => {
+    const collection = client.db("tiein").collection("student");
+    const student = await collection.insertOne(body);
+    client.close();
+
+
+    res.send(student)
+  });
+
+});
+
+//Update data
+app.put('/student', (req, res) => {
+
+
+  client.connect(async err => {
+    const collection = client.db("tiein").collection("student");
+    const student = await collection.findOneAndUpdate(
+      { _id: req.body.id },
+      {
+        $set: {
+          name: req.body.name,
+          class: req.body.class,
+          lastName: req.body.lastName
+        }
+      }
+    );
+
+    client.close();
+
+
+    res.send(student)
+  });
+
+});
+
+//Delete data
+app.delete('/student', (req, res) => {
+
+  
+
+  client.connect(async err => {
+    const collection = client.db("tiein").collection("student");
+    const student = await collection.deleteOne(
+      { name: req.body.name }
+    );
+    client.close();
+
+
+    res.send(student)
+  });
+
+});
+
