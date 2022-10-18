5555269b11576a316d4e6bf49f68ec6132d74938
rojintag <106949675+rojintag@users.noreply.github.com>
2022-10-18 12:25:15 -0700

    feat: Add UserType Component

--- tie-in-frontend/src/App.js
+++ tie-in-frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
- 
+
+
 
 
 function App() {
-   
-  
+
+  const onClickHandel = () => console.log('You clicked me');
   return (
     <div className="App">
       <header className="App-header">
