8d2b09ce13db7ac8b02902d0fe8cd460cca25218
floridajoca <florida.joca@gmail.com>
2022-10-18 23:17:35 -0700

    feat: LUNA-70 upload student project

---
+++ tie-in-frontend/src/api/studentProject.js
@@ -0,0 +1,11 @@
+import project from "./baseURL";
+
+export const requestStudentProjects = () => {
+    return project.get("/studentProject").then((res) => {
+        return res.data.result;
+    });
+}
+
+export const saveStudentProject = (postData) => project.post('/studentProject', postData, {'Content-type': 'application/json'}).then((res) => {
+    return res.data;
+});
