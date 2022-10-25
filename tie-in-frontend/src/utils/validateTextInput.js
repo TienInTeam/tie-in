4151b63b0b5ab030775059c98f84463ecdd13411
LOPESWI3 <104600109+LOPESWI3@users.noreply.github.com>
2022-10-25 12:11:33 -0700

    Validations for student project upload

---
+++ tie-in-frontend/src/utils/validateTextInput.js
@@ -0,0 +1,15 @@
+//function to validate the given input is text only and does not include invalid characters
+const validateTextInput =(input) =>
+{
+        var regInput = /^[A-Za-z\s]*$/;
+            if (!regInput.test(input)) {
+                return false;
+            }
+            else {
+                return true;
+            }
+
+}
+
+export default validateTextInput;
+//Reference https://codingbeautydev.com/blog/javascript-check-if-string-contains-only-letters-and-spaces/
