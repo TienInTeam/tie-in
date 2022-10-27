0ee7732cd03804b31cd56b77b6022e2737e8f1ca
rojintag <106949675+rojintag@users.noreply.github.com>
2022-10-27 12:36:17 -0700

    fix: change the update and findone endpoints

--- index.js
+++ index.js
@@ -14,77 +14,80 @@ const client = new MongoClient(uri);
 app.use(express.json());
 
 
-//Create endpoints
+///////////Create endpoints///////////
+///////////////Student//////////////
 
 //Get all Students
 app.get('/student', (req, res) => {
-
   client.connect(async err => {
     const collection = client.db("TestDB1").collection("Students");
     const query = {};
     const allStudents = await collection.find(query).toArray();
-
     client.close();
-
     res.status(200).send(allStudents);
+  });
+});
+
+//Get one student
+app.get('/student/:id', (req, res) => {
+  client.connect(async err => {
+    const collection = client.db("TestDB1").collection("Students");
+    const oneStudent = await collection.findOne(
+      { _id: new ObjectId(req.params.id) }
+    );
+    client.close();
+    res.status(200).send(oneStudent);
   });
 });
 
 //Create new Student
 app.post('/student', (req, res) => {
-
-  const body = req.body
-
   client.connect(async err => {
     const collection = client.db("TestDB1").collection("Students");
     const student = await collection.insertOne({
-      name: req.body.name,
-      lastName: req.body.lastName,
-      class: req.body.class,
-      age: req.body.age
+      email: req.body.email,
+      fullName: req.body.fullname,
+      Institution: req.body.institution,
+      linkedInUrl: req.body.linkedInUrl,
+      githubUrl: req.body.githubUrl,
+      profileUrl: req.body.profileUrl
     });
     client.close();
-
     res.send(student)
   });
-
 });
 
 //Update student data
 app.put('/student/:id', (req, res) => {
-
   client.connect(async err => {
     const collection = client.db("TestDB1").collection("Students");
     const student = await collection.findOneAndUpdate(
-      { id: req.params.id },
+      { _id: new ObjectId(req.params.id) },
       {
         $set: {
-          name: req.body.name,
-          class: req.body.class,
-          lastName: req.body.lastName
+          email: req.body.email,
+          fullName: req.body.fullName,
+          Institution: req.body.Institution,
+          linkedInUrl: req.body.linkedInUrl,
+          githubUrl: req.body.githubUrl,
+          profileUrl: req.body.profileUrl
         }
       }
     );
-
     client.close();
-
     res.send(student)
   });
-
 });
 
 //Delete data
 app.delete('/student/:id', (req, res) => {
-
   client.connect(async err => {
     const collection = client.db("TestDB1").collection("Students");
     const deleteResponse = await collection.deleteOne(
       { _id: new ObjectId(req.params.id) }
     );
     client.close();
-
     res.send(deleteResponse)
   });
-
 });
 
