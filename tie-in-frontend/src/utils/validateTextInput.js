f5000ba7848ab6945dbd7c93ab0a87d5c2170bb0
LOPESWI3 <104600109+LOPESWI3@users.noreply.github.com>
2022-10-27 10:32:04 -0700

    Formatting 2: Validations for student project upload

--- tie-in-frontend/src/utils/validateTextInput.js
+++ tie-in-frontend/src/utils/validateTextInput.js
@@ -1,13 +1,12 @@
 //function to validate the given input is text only and does not include invalid characters
-const validateTextInput =(input) =>
-{
-        var regInput = /^[A-Za-z\s]*$/;
-            if (!regInput.test(input)) {
-                return false;
-            }
-            else {
-                return true;
-            }
+const validateTextInput = (input) => {
+    var regInput = /^[A-Za-z\s]*$/;
+    if (!regInput.test(input)) {
+        return false;
+    }
+    else {
+        return true;
+    }
 }
 
 export default validateTextInput;
