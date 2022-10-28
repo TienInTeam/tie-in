a836b46a988f41a5d36263076db7d3bae259d891
LOPESWI3 <104600109+LOPESWI3@users.noreply.github.com>
2022-10-27 19:36:55 -0700

    Fix: URL validation and input label correction

--- tie-in-frontend/src/utils/validateURL.js
+++ tie-in-frontend/src/utils/validateURL.js
@@ -1,6 +1,11 @@
 export const isURLValid = (websiteURL) => {
-    const re = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
-    return re.test(websiteURL);
+    let url;
+    try {
+      url = new URL(websiteURL);
+    } catch (_) {
+      return false;
+    }
+    return url.protocol === "http:" || url.protocol === "https:";
 };
 
-//Reference https://www.regextester.com/94502
+//Reference https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
