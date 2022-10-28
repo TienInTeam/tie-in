03e03aa64cec3f9e7b73df0957ec26a661c0f8f7
FloridaLangara <fjoca00@mylangara.ca>
2022-10-27 20:12:45 -0700

    feat: Add Dashboard page for companies login

--- tie-in-frontend/src/pages/Dashboard.js
+++ tie-in-frontend/src/pages/Dashboard.js
@@ -1,8 +1,42 @@
+import {useQuery} from "@tanstack/react-query";
 import React from 'react';
+import {requestStudentProjects} from "../api/studentProject";
+import HighlightedStudentProject from "../components/HighlightedStudentProject";
 
 const Dashboard = () => {
+
+  const requestStudentProject = useQuery([ "studentProject"], () => requestStudentProjects())
+  if (requestStudentProject.isLoading) {
+    return <span>Loading...</span>
+  }
+
+  if (requestStudentProject.isError) {
+    return <span>Error: {requestStudentProject.error.message}</span>
+  }
+
+  const onSeeMore = () => {
+    alert("see more is clicked")
+  }
+
   return (
-    <div>Dashboard</div>
+      <>
+      <div className={"data-visualization"}>
+        <h1>Data visualization 1</h1>
+        <h1>Data visualization 2</h1>
+      </div>
+    <div className={"student-project-wrapper"}>
+      <div className={"title-wrapper"}>
+        <h2>Name</h2>
+        <h2>Description</h2>
+        <h2>Description</h2>
+        <h2>Category</h2>
+        <h2>Location</h2>
+      </div>
+      {requestStudentProject.data.map((student) => (
+          <HighlightedStudentProject studentProject={student} onSeeMore={onSeeMore}/>
+      ))}
+    </div>
+      </>
   )
 }
 
