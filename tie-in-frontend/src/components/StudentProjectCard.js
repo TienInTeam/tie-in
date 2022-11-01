f5491f395eefb392a4267eb3afe3310fc36e2d05
rojintag <106949675+rojintag@users.noreply.github.com>
2022-11-01 12:47:55 -0700

    LUNA-92 feat: Using useQuery hook and requestStudentProjects function

--- tie-in-frontend/src/components/StudentProjectCard.js
+++ tie-in-frontend/src/components/StudentProjectCard.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import {useQuery} from "@tanstack/react-query";
+import {requestStudentProjects} from "../api/studentProject";
 
 function StudentProjectCard() {
+  const requestStudentProject = useQuery([ "studentProject"], () => requestStudentProjects())
+  if (requestStudentProject.isLoading) {
+    return <span>Loading...</span>
+  }
+
+  if (requestStudentProject.isError) {
+    return <span>Error: {requestStudentProject.error.message}</span>
+  }
   return (
     <div>
       
