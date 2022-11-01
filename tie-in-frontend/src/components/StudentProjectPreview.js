aca089cc2948532bd43c9f139d0314516f0e1bd1
rojintag <106949675+rojintag@users.noreply.github.com>
2022-11-01 15:57:34 -0700

    LUNA-92 style: style the component StudentProjectPreview

--- tie-in-frontend/src/components/StudentProjectPreview.js
+++ tie-in-frontend/src/components/StudentProjectPreview.js
@@ -1,23 +1,34 @@
 import React from 'react'
 import Button from './Button';
 
-function StudentProjectPreview({ studentProject, onSeeMore, onEdit }) {
+function StudentProjectPreview({ studentProject, onSeeMore }) {
   const { logo, title, institution, description, category, location } = studentProject;
 
   return (
-    <div>
-      <img src={logo} alt="project's logo" />
-      <h2>{title}</h2>
-      <h3>Institution</h3>
-      <p>{institution}</p>
-      <h3>Description</h3>
-      <p>{description}</p>
-      <h3>Category</h3>
-      <p>{category}</p>
-      <h3>Location</h3>
-      <p>{location}</p>
-      <Button onClick={onSeeMore} />
-      <Button onClick={onEdit} />
+    <div className="student-project-preview">
+      <div className="title-container">
+        <img src={logo} alt="project's logo" />
+        <h2>{title}</h2>
+      </div>
+      <div className="body-container">
+        <div>
+          <h3>Institution</h3>
+          <p>{institution}</p>
+        </div>
+        <div>
+          <h3>Description</h3>
+          <p>{description}</p>
+        </div>
+        <div>
+          <h3>Category</h3>
+          <p>{category}</p>
+        </div>
+        <div>
+          <h3>Location</h3>
+          <p>{location}</p>
+        </div>
+        <Button label={"SeeMore"} variant={"button"} onClick={onSeeMore} />
+      </div>
     </div>
   );
 }
