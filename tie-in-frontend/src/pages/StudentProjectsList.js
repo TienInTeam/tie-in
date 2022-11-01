89ad55173d6a770fad40e5a5885e26a1f1a0456e
rojintag <106949675+rojintag@users.noreply.github.com>
2022-11-01 13:21:46 -0700

    LUNA-92 feat: Show the data of student's projects in studentProjectsList page

--- tie-in-frontend/src/pages/StudentProjectsList.js
+++ tie-in-frontend/src/pages/StudentProjectsList.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import {useQuery} from "@tanstack/react-query";
+import {requestStudentProjects} from "../api/studentProject";
+import StudentProjectPreview from "../components/StudentProjectPreview";
 
 function StudentProjectsList() {
+  const requestStudentProject = useQuery(["studentProject"], () => requestStudentProjects())
+  if (requestStudentProject.isLoading) {
+    return <span>Loading...</span>
+  }
+
+  if (requestStudentProject.isError) {
+    return <span>Error: {requestStudentProject.error.message}</span>
+  }
+
+  const onSeeMore = () => {
+    alert("see more is clicked")
+  }
   return (
-    <div>StudentProjectsList</div>
+    <div className="student-projects-list">
+    <div>
+      {requestStudentProject.data.map((student) => (
+            <StudentProjectPreview studentProject={student} onSeeMore={onSeeMore}/>
+        ))}
+    </div>
+  </div>
   )
 }
 
-export default StudentProjectsList
+export default StudentProjectsList;
