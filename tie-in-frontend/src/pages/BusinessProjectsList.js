9a587cd7093c0dc2d92476e9196c8ae3d399ba4e
rojintag <106949675+rojintag@users.noreply.github.com>
2022-11-01 17:38:04 -0700

    LUNA-96 feat: Add useQuery hook and requestBusinessProject hook in BusinessProjectList page

--- tie-in-frontend/src/pages/BusinessProjectsList.js
+++ tie-in-frontend/src/pages/BusinessProjectsList.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import {useQuery} from "@tanstack/react-query";
+import {requestBusinessProjects} from "../api/businessProject";
 
 function BusinessProjectsList() {
+  const requestBusinessProject = useQuery(["businessProject"], () => requestBusinessProjects())
+  if (requestBusinessProject.isLoading) {
+    return <span>Loading...</span>
+  }
+
+  if (requestBusinessProject.isError) {
+    return <span>Error: {requestBusinessProject.error.message}</span>
+  }
   return (
     <div>
       
