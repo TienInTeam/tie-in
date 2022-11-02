c479769652c1f1d50eafeee0791d488b5e02a30c
rojintag <106949675+rojintag@users.noreply.github.com>
2022-11-01 23:09:28 -0700

    LUNA-96 feat: Use the component of BusinessProjectPreview and pass the properties

--- tie-in-frontend/src/pages/BusinessProjectsList.js
+++ tie-in-frontend/src/pages/BusinessProjectsList.js
@@ -1,5 +1,7 @@
-import {useQuery} from "@tanstack/react-query";
-import {requestBusinessProjects} from "../api/businessProject";
+import { useQuery } from "@tanstack/react-query";
+import { requestBusinessProjects } from "../api/businessProject";
+import BusinessProjectPreview from "../components/BusinessProjectPreview";
+
 
 function BusinessProjectsList() {
   const requestBusinessProject = useQuery(["businessProject"], () => requestBusinessProjects())
@@ -10,11 +12,20 @@ function BusinessProjectsList() {
   if (requestBusinessProject.isError) {
     return <span>Error: {requestBusinessProject.error.message}</span>
   }
+
+  const onSeeMore = () => {
+    alert("see more is clicked")
+  }
+
   return (
     <div>
-      
+      <div>
+        {requestBusinessProject.data.map((businessProject, index) => (
+          <BusinessProjectPreview businessProject={businessProject} key={index} onSeeMore={onSeeMore} />
+        ))}
+      </div>
     </div>
   )
 }
 
-export default BusinessProjectsList
+export default BusinessProjectsList;
