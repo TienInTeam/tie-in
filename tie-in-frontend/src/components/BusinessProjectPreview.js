1cc06298e1dd001b54ffcb28baa3d30bc46b7973
rojintag <106949675+rojintag@users.noreply.github.com>
2022-11-02 10:17:13 -0700

    LUNA-96 style: Adding some styles to the BusinessProjectPreview component

--- tie-in-frontend/src/components/BusinessProjectPreview.js
+++ tie-in-frontend/src/components/BusinessProjectPreview.js
@@ -2,23 +2,34 @@ import React from 'react';
 import Button from './Button';
 
 function BusinessProjectPreview({ businessProject, onSeeMore }) {
-  const { logo, company_name, description, teamRequirement, dueDate, category, location } = businessProject;
+  const { logo, company_name, description, team_member, dueDate, category, location } = businessProject;
   return (
-    <div>
+    <div className="business-project-preview">
+      <div className="title-container">
        <img src={logo} alt="project's logo" />
       <h2>{company_name}</h2>
-      <h3>Description</h3>
-      <p>{description}</p>
-      <h3>Team Requirement</h3>
-      <p>{teamRequirement}</p>
-      <h3>Due date</h3>
-      <p>{dueDate}</p>
-      <h3>Category</h3>
-      <p>{category}</p>
-      <h3>Location</h3>
-      <p>{location}</p>
-
-      <Button label={"SeeMore"} variant={"button"} onClick={onSeeMore} />
+      </div>
+      <div className="body-container">
+      <div>
+        <h3>Description</h3>
+        <p>{description}</p>
+      </div>
+      <div>
+        <h3>Team Member</h3>
+        <p>{team_member}</p>
+        <h3>Due date</h3>
+        <p>{dueDate}</p>
+      </div>
+      <div>
+        <h3>Category</h3>
+        <p>{category}</p>
+      </div>
+      <div>
+        <h3>Location</h3>
+        <p>{location}</p>
+        <Button label={"SeeMore"} variant={"button"} onClick={onSeeMore} />
+      </div>
+      </div>
     </div>
   )
 }
