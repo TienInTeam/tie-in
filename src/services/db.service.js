77a96203c58b2b0a49e766c0da41c566bf41a43e
Diego Santa Cruz <dsantacruzmeoo00@mylangara.ca>
2022-11-02 09:13:21 -0700

    Refactor: Added services and model layers

--- src/services/db.service.js
+++ src/services/db.service.js
@@ -1,31 +1,15 @@
-
 //Import DB configuration file
-const dbConfig = require('../configs/db.config');
+const dbConfig = require("../configs/db.config");
 
 //Connect mongoDB
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient } = require("mongodb");
 const client = new MongoClient(dbConfig.db.uri);
 
-async function query(sql, params) {
-    const connection = await mysql.createConnection(dbConfig);
-    const [results, ] = await connection.execute(sql, params);
-  
-    return results;
-  }
-
-
-const collection = client.db("TestDB1").collection("Users");
-const query = {};
-const allUsers = await collection.find(query).toArray();
-client.close();
-
-async function query(sql, params) {
-  const connection = await mysql.createConnection(dbConfig);
-  const [results, ] = await connection.execute(sql, params);
-
-  return results;
+async function connectToDatabaseCollection(collection) {
+  const database = client.db(dbConfig.db.database);
+  const collection = database.collection(collection);
 }
 
 module.exports = {
-  query
-}
+  connectToDatabaseCollection,
+};
