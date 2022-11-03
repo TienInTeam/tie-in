fece861c9390ea22bd010a59ff4ade30d6b15547
FloridaLangara <fjoca00@mylangara.ca>
2022-11-02 19:21:47 -0700

    feat: Add the API call to add and search student

--- tie-in-frontend/src/api/baseURL.js
+++ tie-in-frontend/src/api/baseURL.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const project = axios.create({
     baseURL: "http://localhost:3001"
+    // baseURL: "https://tie-in-dev.up.railway.app"
 });
 
 export default project;
