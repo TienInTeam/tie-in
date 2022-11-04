01c99e837b5d7c8722633c03df48d757885d2b4e
FloridaLangara <fjoca00@mylangara.ca>
2022-11-04 13:04:04 -0700

    fix: Fix coding structure

--- tie-in-frontend/src/components/BusinessProjectPreview.js
+++ tie-in-frontend/src/components/BusinessProjectPreview.js
@@ -3,7 +3,7 @@ import Button from './Button';
 
 function BusinessProjectPreview({ businessProject, onSeeMore, onCheckStatus }) {
   const { status, logo, company_name, description, team_member, dueDate, category, location } = businessProject;
-  console.log(Boolean(businessProject.status));
+
   return (
     <div className="business-project-preview">
       <div className="title-container">
@@ -24,19 +24,28 @@ function BusinessProjectPreview({ businessProject, onSeeMore, onCheckStatus }) {
         </div>
         <div>
           <h3>Category</h3>
-          {category ? category.map((cat, index) => { return (<div key={index} className="category">{cat}</div>) }) : <p>-</p>}      </div>
+          {category ? category.map((cat, index) => {
+            return (
+                <div
+                    key={index}
+                    className="category"
+                >
+                  {cat}
+                </div>) }) : <p>-</p>}
+        </div>
         <div className="button-container">
           <h3>Location</h3>
           <p>{location}</p>
           <Button label={"See More"} variant={"primary"} onClick={onSeeMore} />
           {status ? 
-          <Button label={"Check Status"} variant={"secondary"} onClick={onCheckStatus} />
-           : <p>salam</p>
-           }
-
+          <Button
+              label={"Check Status"}
+              variant={"secondary"}
+              onClick={onCheckStatus}
+          /> : "" }
         </div>
       </div>
-    </div >
+    </div>
   )
 }
 
