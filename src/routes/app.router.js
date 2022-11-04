d18808bec979e617f46bd11218615a9057ed8ab4
Diego Santa Cruz <dsantacruzmeoo00@mylangara.ca>
2022-11-04 05:00:55 -0700

    refactor: getAll and getOne operations working

--- src/routes/app.router.js
+++ src/routes/app.router.js
@@ -3,25 +3,31 @@ const router = express.Router();
 const appController = require('../controllers/app.controller');
 
 ////////// USER ROUTES //////////
-router.get('/', appController.get);
+router.get('/users', appController.getAllUsers);
+router.get('/users/:uid', appController.getOneUserByUid);
 
 ////////// STUDENT ROUTES //////////
-router.get('/', appController.get);
+router.get('/students', appController.getAllStudents);
+router.get('/students/:email', appController.getOneStudentByEmail);
 
 ////////// TEAMS ROUTES //////////
-router.get('/', appController.get);
+router.get('/teams', appController.getAllTeams);
+router.get('/teams/:name', appController.getOneTeamByName);
 
 ////////// BUSINESS ROUTES //////////
-router.get('/', appController.get);
+router.get('/business', appController.getAllBusiness);
+router.get('/business/:email', appController.getOneBusinessByEmail);
 
 ////////// STUDENT PROJECT ROUTES //////////
-router.get('/', appController.get);
+router.get('/students/projects', appController.getAllStudentProjects);
+router.get('/students/projects/:name', appController.getOneStudentProjectByName);
 
 ////////// BUSINESS PROJECT ROUTES //////////
-router.get('/', appController.get);
+router.get('/business/projects', appController.getAllBusinessProjects);
+router.get('/business/projects/:name', appController.getOneBusinessProjectByName);
 
 ////////// APPLICATIONS ROUTES //////////
-router.get('/', appController.get);
+router.get('/students/teams/applications', appController.getAllApplications);
 
 
 module.exports = router;
--- src/services/app.service.js
+++ src/services/app.service.js
@@ -2,53 +2,132 @@ const dbService = require("./db.service");
 
 ////////// USER //////////
 async function getAllUsersFromDb(collection) {
-  const allUsers = dbService.getAllFromDb(collection);
+  const response = dbService.getAllFromDb(collection);
 
-  return allUsers;
+  return response;
+}
+
+async function getOneUserFromDb(collection, userQuery) {
+  const response = dbService.getOneFromDb(collection, userQuery);
+
+  return response;
+}
+
+async function createOneUserInDb(collection, requestBody) {
+  const response = dbService.createOneInDb(collection, requestBody);
+
+  return response;
+}
+
+async function updateOneUserInDb(collection, userQuery, requestBody) {
+  const response = dbService.updateOneInDb(collection, userQuery, requestBody);
+
+  return response;
+}
+
+async function deleteOneUserFromDb(collection, userQuery) {
+  const response = dbService.deleteOneFromDb(collection, userQuery);
+
+  return response;
 }
 
 ////////// STUDENT //////////
-async function getAllUsersFromDb(collection) {
-  const allUsers = dbService.getAllFromDb(collection);
+async function getAllStudentsFromDb(collection) {
+  const response = dbService.getAllFromDb(collection);
 
-  return allUsers;
+  return response;
+}
+
+async function getOneStudentFromDb(collection, userQuery) {
+  const response = dbService.getOneFromDb(collection, userQuery);
+
+  return response;
 }
 
 ////////// TEAMS //////////
-async function getAllUsersFromDb(collection) {
-  const allUsers = dbService.getAllFromDb(collection);
+async function getAllTeamsFromDb(collection) {
+  const response = dbService.getAllFromDb(collection);
 
-  return allUsers;
+  return response;
+}
+
+async function getOneTeamFromDb(collection, userQuery) {
+  const response = dbService.getOneFromDb(collection, userQuery);
+
+  return response;
 }
 
 ////////// BUSINESS //////////
-async function getAllUsersFromDb(collection) {
-  const allUsers = dbService.getAllFromDb(collection);
+async function getAllBusinessFromDb(collection) {
+  const response = dbService.getAllFromDb(collection);
 
-  return allUsers;
+  return response;
+}
+
+async function getOneBusinessFromDb(collection, userQuery) {
+  const response = dbService.getOneFromDb(collection, userQuery);
+
+  return response;
 }
 
 ////////// STUDENT PROJECT //////////
-async function getAllUsersFromDb(collection) {
-  const allUsers = dbService.getAllFromDb(collection);
+async function getAllStudentProjectsFromDb(collection) {
+  const response = dbService.getAllFromDb(collection);
 
-  return allUsers;
+  return response;
+}
+
+async function getOneStudentProjectFromDb(collection, userQuery) {
+  const response = dbService.getOneFromDb(collection, userQuery);
+
+  return response;
 }
 
 ////////// BUSINESS PROJECT //////////
-async function getAllUsersFromDb(collection) {
-  const allUsers = dbService.getAllFromDb(collection);
+async function getAllBusinessProjectsFromDb(collection) {
+  const response = dbService.getAllFromDb(collection);
 
-  return allUsers;
+  return response;
 }
 
-////////// USER //////////
-async function getAllUsersFromDb(collection) {
-  const allUsers = dbService.getAllFromDb(collection);
+async function getOneBusinessProjectFromDb(collection, userQuery) {
+  const response = dbService.getOneFromDb(collection, userQuery);
 
-  return allUsers;
+  return response;
+}
+
+////////// APPLICATION //////////
+async function getAllApplicationsFromDb(collection) {
+  const response = dbService.getAllFromDb(collection);
+
+  return response;
+}
+
+async function getOneApplicationFromDb(collection, userQuery) {
+  const response = dbService.getOneFromDb(collection, userQuery);
+
+  return response;
 }
 
 module.exports = {
   getAllUsersFromDb,
+  getOneUserFromDb,
+
+  getAllStudentsFromDb,
+  getOneStudentFromDb,
+
+  getAllTeamsFromDb,
+  getOneTeamFromDb,
+
+  getAllBusinessFromDb,
+  getOneBusinessFromDb,
+
+  getAllStudentProjectsFromDb,
+  getOneStudentProjectFromDb,
+
+  getAllBusinessProjectsFromDb,
+  getOneBusinessProjectFromDb,
+
+  getAllApplicationsFromDb,
+  getOneApplicationFromDb,
 };
