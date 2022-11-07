4b2ee8b7ea5ebe6fcf0ee10790c2ee923e7747e7
Diego Santa Cruz <dsantacruzmeoo00@mylangara.ca>
2022-11-07 09:23:45 -0800

    feature: Adding properties to Student Model

--- src/models/student.model.js
+++ src/models/student.model.js
@@ -2,22 +2,24 @@ function Student(
   email,
   firstName,
   lastName,
+  location,
   institution,
   linkedInUrl,
   githubUrl,
   portfolioUrl,
-  position,
-  phoneNumber
+  position
 ) {
   this.email = email;
   this.first_name = firstName;
   this.last_name = lastName;
+  //Student city
+  this.location = location;
   this.institution = institution;
   this.linkedIn_url = linkedInUrl;
   this.github_url = githubUrl;
   this.portfolio_url = portfolioUrl;
+  //Student expertise (Backend, front, designer, etc)
   this.position = position;
-  this.phone_number = phoneNumber;
 }
 
 module.exports = Student;
