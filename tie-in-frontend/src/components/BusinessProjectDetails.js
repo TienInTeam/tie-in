ea07351fe25a9eba479a1d0875cc201ccefb650e
LOPESWI3 <104600109+LOPESWI3@users.noreply.github.com>
2022-11-10 12:56:41 -0800

    styling changes

--- tie-in-frontend/src/components/BusinessProjectDetails.js
+++ tie-in-frontend/src/components/BusinessProjectDetails.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Button from './Button';
-import { ReactComponent as FileIcon } from '../assets/icons/others/attach-icon.svg';
+import { ReactComponent as FileIcon } from '../assets/icons/others/file-icon.svg';
 
 
 
 function BusinessProjectDetails({ businessProject, business, onApply }) {
-  const { name, additiona_file_url, location, description, budget, team_size, team_requirements, start_date, end_date, subjects, category, technology, additional_field, projectTitle, links, summary, file } = businessProject;
+  const { name, additional_file_url, location, description, budget, team_size, team_requirements, start_date, end_date, subjects, category, technology, additional_field, projectTitle, links, summary, file } = businessProject;
   const { logo, company_name } = business;
 
   return (
@@ -20,16 +20,28 @@ function BusinessProjectDetails({ businessProject, business, onApply }) {
       <div className="business-request-body">
         <div className="request-body-top">
           <h2>{name}</h2>
-          {additiona_file_url ? <p>{<a href={additiona_file_url}><FileIcon /> <span>File (additional information)</span></a>}</p> : <></>}
         </div>
-        <p>Team members: <span>{team_size}</span></p>
-        <p>Team requirement: <span>{team_requirements}</span></p>
-        <p>Project Duration: <span>{start_date} to {end_date}</span> </p>
-        <p>Estimated Budget: <span>{budget}</span></p>
-        <h3>Description</h3>
-        <p>{description}</p>
-        <h3>Additional Field Text</h3>
-        <p>{additional_field ? additional_field : "No additional data available"}</p>
+        <div className="request-details">
+          <p>Team members: <span>{team_size}</span></p>
+          <p>Team requirement: <span>{team_requirements}</span></p>
+          <p>Project Duration: <span>{start_date} to {end_date}</span> </p>
+          <p>Estimated Budget: <span>{budget}</span></p>
+        </div>
+        <div className="description">
+          <h3>Description</h3>
+          <p>{description}</p>
+        </div>
+        <div className="additional-info">
+          <h3>Additional Information</h3>
+          <p>{additional_field ? additional_field : "No additional data available"}</p>
+        </div>
+        <div className='additional-files'>
+          {
+            additional_file_url.map((files, index) =>
+              <a key={index} href={files}><FileIcon /> <span>{files.split('/').pop()}</span></a>
+            )
+          }
+        </div>
         <h3>Links</h3>
         <a href={links}>{links}</a>
       </div>
