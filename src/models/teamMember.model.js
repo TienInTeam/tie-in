abfc215b950183cfae887d0d6b38ccf8b0b9872c
Diego Santa Cruz <dsantacruzmeoo00@mylangara.ca>
2022-11-14 04:06:16 -0800

    feat: Added get all student's teams by id.

---
+++ src/models/teamMember.model.js
@@ -0,0 +1,7 @@
+function Member(studentId, studentName, studentPhoto) {
+  this.student_id = studentId;
+  this.student_name = studentName;
+  this.student_photo = studentPhoto;
+}
+
+module.exports = Member;
