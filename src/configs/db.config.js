e3efec6529be9f757b12a4d24cda8c67fc322035
Diego Santa Cruz <dsantacruzmeoo00@mylangara.ca>
2022-11-14 04:03:32 -0800

    refactor: change db config

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 const settings = {
-  dbName: "TestDB1",
-  uri: "mongodb+srv://tieinuser1:tieinmongo01@testcluster.ryzz4av.mongodb.net/?retryWrites=true&w=majority",
+  dbName: "TESTDB1",
+  uri: "mongodb+srv://root:root@cluster0.lb9myjr.mongodb.net/?retryWrites=true&w=majority",
 };
 
 module.exports = {
