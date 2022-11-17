b6511f5c17c2a8b67ed2160eb8647ac755952d30
rojintag <106949675+rojintag@users.noreply.github.com>
2022-11-17 14:27:35 -0800

    LUNA-109 feat: Add function of search

---
+++ tie-in-frontend/src/pages/Search.js
@@ -0,0 +1,47 @@
+import { useQuery } from "@tanstack/react-query";
+import { useLocation, useParams } from "react-router-dom";
+import { requestBusinessProjectsByQuery, requestBusinessProjects } from "../api/businessProject";
+import BusinessProjectPreview from "../components/BusinessProjectPreview";
+
+function Search() {
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  // let params = useParams();
+  const query = queryParams.get('q');
+  console.log(query);
+
+
+  const request = useQuery(["businessProject"], () => requestBusinessProjectsByQuery(query),
+    {
+      onError: (error) => {
+        alert(error.message);
+      }
+    },
+    { enabled: !!query }
+  );
+  console.log(request.data, 'in request data e');
+
+  const onSeeMore = () => {
+    alert("see more is clicked")
+  }
+  const onCheckStatus = () => {
+    alert("Check status is clicked")
+  }
+
+  return (
+    <div>
+      <h2>Projects Including "{query}"</h2>
+      {
+        request.data.map((business, index) =>
+          <BusinessProjectPreview
+            businessProject={business}
+            key={index}
+            onSeeMore={onSeeMore}
+            onCheckStatus={onCheckStatus}
+          />
+        )}
+    </div>
+  )
+}
+
+export default Search
