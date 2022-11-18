bdc95f4e0d7c7fa2e812f36d46afd47b6409c02e
rojintag <106949675+rojintag@users.noreply.github.com>
2022-11-18 12:24:52 -0800

    LUNA-109 feat: Adding icon search

--- tie-in-frontend/src/components/Searchbar.js
+++ tie-in-frontend/src/components/Searchbar.js
@@ -1,17 +1,19 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom";
-import InputType from "./InputType"
+import InputType from "./InputType";
+import { ReactComponent as SearchIcon } from '../assets/icons/actions/actions-search.svg';
 
-function Searchbar({onCategory, onSize, onLocation}) {
+
+function Searchbar({ onCategory, onSize, onLocation }) {
   const [term, setTerm] = useState("");
-  
+
 
   const history = useNavigate();
-  const onSearch= (e) => {
+  const onSearch = (e) => {
     setTerm(e.target.value)
   }
-  const submitHandle= () => {
-    history(`/search?q=${term}`, {replace: true });
+  const submitHandle = () => {
+    history(`/search?q=${term}`, { replace: true });
   }
   return (
     <div className="search-bar">
@@ -19,37 +21,38 @@ function Searchbar({onCategory, onSize, onLocation}) {
         <h1>Business Project</h1>
         <p>Find the detail requests here</p>
       </div>
-      <form onSubmit={submitHandle}>
-       <InputType type={"text"} placeHolder={"Input Project Name"} onChange={onSearch}/>
+      <form className="search-wrapper" onSubmit={submitHandle}>
+      <div className="icon search-icon" onClick={submitHandle}><SearchIcon /></div>
+        <InputType type={"text"} placeHolder={"Input Project Name"} onChange={onSearch} />
       </form>
-            <div className="filter-wrapper">
-              <select id="category" aria-label="Choose category" onChange={onCategory}>
-                  <option selected disabled>Category</option>
-                  <option value="UI/UX">UI/UX</option>
-                  <option value="Mobile Application">Mobile Application</option>
-                  <option value="System Architecture">System Architecture</option>
-                  <option value="Web Development">Web Development</option>
-                  <option value="Commercial">Commercial</option>
-                  <option value="Marketing">Marketing</option>
-              </select>
-              <select id="size" aria-label="Choose size" onChange={onSize}>
-                  <option selected disabled>Location</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-              </select>
-              <select id="location" aria-label="Choose location" onChange={onLocation}>
-                  <option selected disabled>Size</option>
-                  <option value="Vancouver">Vancouver</option>
-                  <option value="Burnaby">Burnaby</option>
-                  <option value="North Vancouver">North Vancouver</option>
-                  <option value="Remotely">Remotely</option>
-                  <option value="Out of BC">Out of BC</option>
-              </select>
-            </div>
+      <form onSubmit={submitHandle} className="filter-wrapper">
+        <select id="category" aria-label="Choose category" onChange={onCategory}>
+          <option selected disabled>Category</option>
+          <option value="UI/UX">UI/UX</option>
+          <option value="Mobile Application">Mobile Application</option>
+          <option value="System Architecture">System Architecture</option>
+          <option value="Web Development">Web Development</option>
+          <option value="Commercial">Commercial</option>
+          <option value="Marketing">Marketing</option>
+        </select>
+        <select id="size" aria-label="Choose size" onChange={onSize}>
+          <option selected disabled>Size</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+        </select>
+        <select id="location" aria-label="Choose location" onChange={onLocation}>
+          <option selected disabled>Location</option>
+          <option value="Vancouver">Vancouver</option>
+          <option value="Burnaby">Burnaby</option>
+          <option value="North Vancouver">North Vancouver</option>
+          <option value="Remotely">Remotely</option>
+          <option value="Out of BC">Out of BC</option>
+        </select>
+      </form >
     </div>
   )
 }
