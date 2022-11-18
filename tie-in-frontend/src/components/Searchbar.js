b25bb6d527f59ce65d691c746c6def94b5aa883f
rojintag <106949675+rojintag@users.noreply.github.com>
2022-11-18 08:22:22 -0800

    LUNA-109 feat: Adding selective drop down for search

--- tie-in-frontend/src/components/Searchbar.js
+++ tie-in-frontend/src/components/Searchbar.js
@@ -2,8 +2,10 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom";
 import InputType from "./InputType"
 
-function Searchbar({}) {
+function Searchbar({onCategory, onSize, onLocation}) {
   const [term, setTerm] = useState("");
+  
+
   const history = useNavigate();
   const onSearch= (e) => {
     setTerm(e.target.value)
@@ -16,6 +18,32 @@ function Searchbar({}) {
       <form onSubmit={submitHandle}>
        <InputType type={"text"} placeHolder={"Input Project Name"} onChange={onSearch}/>
       </form>
+            <select id="category" aria-label="Choose category" onChange={onCategory}>
+                <option selected disabled>Category</option>
+                <option value="UI/UX">UI/UX</option>
+                <option value="Mobile Application">Mobile Application</option>
+                <option value="System Architecture">System Architecture</option>
+                <option value="Web Development">Web Development</option>
+                <option value="Commercial">Commercial</option>
+                <option value="Marketing">Marketing</option>
+            </select>
+            <select id="size" aria-label="Choose size" onChange={onSize}>
+                <option selected disabled>Location</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+            </select>
+            <select id="location" aria-label="Choose location" onChange={onLocation}>
+                <option selected disabled>Size</option>
+                <option value="Vancouver">Vancouver</option>
+                <option value="Burnaby">Burnaby</option>
+                <option value="North Vancouver">North Vancouver</option>
+                <option value="Remotely">Remotely</option>
+                <option value="Out of BC">Out of BC</option>
+            </select>
     </div>
   )
 }
