5b110087e6a6be88be1c39b6bb8bdb1ad75b8ac6
rojintag <106949675+rojintag@users.noreply.github.com>
2022-11-18 08:22:22 -0800

    LUNA-109 feat: Adding selective drop down for search

--- tie-in-frontend/src/pages/BusinessProjectsList.js
+++ tie-in-frontend/src/pages/BusinessProjectsList.js
@@ -1,10 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { requestBusinessProjects } from "../api/businessProject";
 import BusinessProjectPreview from "../components/BusinessProjectPreview";
 import Searchbar from "../components/Searchbar";
 import SideMenu from "../components/SideMenu";
 
 function BusinessProjectsList() {
+  const [category, setCategory] = useState([]);
+  const [size, setSize] = useState([]);
+  const [location, setLocation] = useState([]);
   const requestBusinessProject = useQuery(["businessProject"], () => requestBusinessProjects(), {
     onError: (error) => {
       alert(error.message);
@@ -22,6 +26,59 @@ function BusinessProjectsList() {
     alert("Check status is clicked")
   }
 
+  const categoryHandle = (e) => setCategory([e.target.value]);
+if(requestBusinessProject.data.category===categoryHandle){
+  return (
+    <div className="grid-container">
+      <SideMenu />
+      <div>
+      <Searchbar onCategory={categoryHandle} onSize={(e) => setSize([e.target.value])} onLocation={(e) => setLocation([e.target.value])} />
+        {requestBusinessProject.data.map((business, index) =>
+          <BusinessProjectPreview
+            businessProject={business}
+            key={index}
+            onSeeMore={onSeeMore}
+            onCheckStatus={onCheckStatus}
+          />
+        )}
+      </div>
+    </div>
+  );
+}else if (requestBusinessProject.data.team_size==="2"){
+  return (
+    <div className="grid-container">
+      <SideMenu />
+      <div>
+      <Searchbar onCategory={(e) => setCategory([e.target.value])} onSize={(e) => setSize([e.target.value])} onLocation={(e) => setLocation([e.target.value])} />
+        {requestBusinessProject.data.map((business, index) =>
+          <BusinessProjectPreview
+            businessProject={business}
+            key={index}
+            onSeeMore={onSeeMore}
+            onCheckStatus={onCheckStatus}
+          />
+        )}
+      </div>
+    </div>
+  );
+}else if (requestBusinessProject.data.location==="Vancouver"){
+  return (
+    <div className="grid-container">
+      <SideMenu />
+      <div>
+      <Searchbar onCategory={(e) => setCategory([e.target.value])} onSize={(e) => setSize([e.target.value])} onLocation={(e) => setLocation([e.target.value])} />
+        {requestBusinessProject.data.map((business, index) =>
+          <BusinessProjectPreview
+            businessProject={business}
+            key={index}
+            onSeeMore={onSeeMore}
+            onCheckStatus={onCheckStatus}
+          />
+        )}
+      </div>
+    </div>
+  );
+}else {
   return (
     <div className="grid-container">
       <SideMenu />
@@ -38,6 +95,7 @@ function BusinessProjectsList() {
       </div>
     </div>
   );
+        }
 }
 
 export default BusinessProjectsList;
