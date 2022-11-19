9818fd88a8b9ceeb4dc1190dbaf4152a9a1618f6
FloridaLangara <fjoca00@mylangara.ca>
2022-11-18 16:18:05 -0800

    fix: Add validation when category empty

--- tie-in-frontend/src/components/HighlightedStudentProject.js
+++ tie-in-frontend/src/components/HighlightedStudentProject.js
@@ -3,11 +3,19 @@ import Button from './Button';
 
 function HighlightedStudentProject({studentProject, onSeeMore}) {
     const {name, institution, category, location} = studentProject;
+
+    const renderCategory = () => {
+        if (!category) {
+            return null;
+        }
+        return category?.map((category, index) =>
+            <span key={index}> {category} </span> )
+    }
     return (
             <div className={"highlightStudentProject"}>
                 <h2>{name}</h2>
                 <div>
-                {category? category.map((cat, index) => {return(<div className="category" key={index}>{cat}</div>)}) : ""}
+                    {renderCategory()}
                 </div>
                 <p>{institution}</p>
                 <p>{location}</p>
