ae71ec4e4a5bfefc694cab7dd53f5f60d7cf9ca8
FloridaLangara <fjoca00@mylangara.ca>
2022-11-22 12:09:14 -0800

    chore:  Remove extra base url

--- tie-in-frontend/src/api/baseURL.js
+++ tie-in-frontend/src/api/baseURL.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const project = axios.create({
  // baseURL: "http://localhost:3001"
-    /// baseURL: "https://tie-in-dev.up.railway.app"
     baseURL: "https://tie-in-render.onrender.com"
 });
 
