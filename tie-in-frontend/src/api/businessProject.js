8be94a2a063267706d779fc7787acfa6da5949da
FloridaLangara <fjoca00@mylangara.ca>
2022-11-24 12:22:19 -0800

    fix: Remove the condition in API calls

--- tie-in-frontend/src/api/businessProject.js
+++ tie-in-frontend/src/api/businessProject.js
@@ -2,9 +2,6 @@ import project from "./baseURL";
 
 export const requestBusinessProjects = () => {
     return project.get("/businessProjects").then((res) => {
-        if(!res.data) {
-            throw new Error("try again");
-        }
         return res.data;
     });
 }
@@ -16,9 +13,6 @@ export const saveBusinessProject = (postData) => project.post('/businessProjects
 
 export const requestBusinessProjectsByID = (id) => {
     return project.get(`/businessProjects/${id}`).then((res) => {
-        if(!res.data) {
-            throw new Error("try again");
-        }
         return res.data;
     });
 }
--- tie-in-frontend/src/api/user.js
+++ tie-in-frontend/src/api/user.js
@@ -2,12 +2,9 @@ import project from "./baseURL";
 
 export const requestUser = (uid) => {
     return project.get(`/users/${uid}`).then((res) => {
-        if(!res.data) {
-            throw new Error("try again");
-        } return res.data;
+        return res.data;
     });
 }
-
 
 export const addUser = (postData) => { project.post('/users', postData,).then((res) => {
     return res.data;
