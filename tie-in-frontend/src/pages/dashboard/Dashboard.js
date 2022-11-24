75605d01ae5a3fc94b62261bf0097db4ec7e58ae
FloridaLangara <fjoca00@mylangara.ca>
2022-11-24 12:24:03 -0800

    chore: Remove console log

--- tie-in-frontend/src/pages/dashboard/Dashboard.js
+++ tie-in-frontend/src/pages/dashboard/Dashboard.js
@@ -1,32 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import React from 'react';
-import {getBusinessByEmail} from "../../api/business";
+
 import {requestUser} from "../../api/user";
 import BusinessDashboard from "./BusinessDashboard";
 import StudentDashboard from "./StudentDashboard";
 
 const Dashboard = () => {
-    const userEmail = sessionStorage.getItem("userEmail");
-
     const requestUserType = useQuery(["user"], () => requestUser(sessionStorage.getItem("userId")), {
         enabled: !!sessionStorage.getItem("userId"),
         onSuccess: (data) => {
-            sessionStorage.setItem("userType", data.type)
-            console.log(sessionStorage.getItem("userType"))
+            sessionStorage.setItem("userType", data.type);
         }
     });
-
-    const requestBusiness = useQuery(['business'], () => getBusinessByEmail(userEmail), {
-        enabled: !!requestUserType
-    })
 
     if (requestUserType.isLoading) {
         return <span>Loading...</span>
     }
 
     if (sessionStorage.getItem('userType') === "business") {
-        sessionStorage.setItem("userMongoId", requestBusiness.data._id);
-        sessionStorage.setItem("userName", requestBusiness.data.name)
         return <BusinessDashboard/>
     } else if (sessionStorage.getItem('userType') === "student"){
         return <StudentDashboard />
