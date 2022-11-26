89c63674b690a7c93f74619becd7366c0a38d2b2
FloridaLangara <fjoca00@mylangara.ca>
2022-11-25 19:48:57 -0800

    fix: Add API calls for student application

--- tie-in-frontend/src/api/studentApplications.js
+++ tie-in-frontend/src/api/studentApplications.js
@@ -1,17 +1,17 @@
 import project from "./baseURL";
 
-export const requestApplicationStatuses = () => {
-    return project.get("/applications").then((res) => {
-        return res.data;
-    });
-}
-
 export const requestApplications = () => {
     return project.get("/applications").then((res) => {
         return res.data;
     });
 }
 
-export const saveStudentApplication = (postData) => project.post('/applications', postData, { 'Content-type': 'application/json' }).then((res) => {
+export const saveStudentApplication = (postData) => project.post('/applications', postData).then((res) => {
     return res.data;
 });
+
+export const getStudentApplication = (studentId) => {
+    project.get(`/applications/student/${studentId}`).then((res) => {
+        return res.data;
+    })
+}
