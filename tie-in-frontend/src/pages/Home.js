3e01bc6c98fa09ed2fcfa8b137ccace6db43f8b3
diego_s <dsantacruzmeoo00@mylangara.ca>
2022-11-26 02:43:33 -0800

    style: Partial Home style

--- tie-in-frontend/src/pages/Home.js
+++ tie-in-frontend/src/pages/Home.js
@@ -28,9 +28,9 @@ const Home = () => {
                     title={"Student Team"}
                     subtitle={"How to upload your project?"}
                     onTryNowClick={onTryNow}
-                    step1={"Step 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium velit in lorem tempus vestibulum non egestas nibh."}
-                    step2={"Step 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium velit in lorem tempus vestibulum non egestas nibh."}
-                    step3={"Step 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium velit in lorem tempus vestibulum non egestas nibh."}
+                    step1={"Step 1: Fill in related documents and information of your project."}
+                    step2={"Step 2: Select your project team to include your team members."}
+                    step3={"Step 3: Review your project details and click “Submit” when you are finished."}
                 />
                 <StudentProject/>
             </div>
@@ -39,9 +39,9 @@ const Home = () => {
                 <HomeSection
                     subtitle={"How to apply projects from company?"}
                     onTryNowClick={onTryNow}
-                    step1={"Step 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium velit in lorem tempus vestibulum non egestas nibh."}
-                    step2={"Step 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium velit in lorem tempus vestibulum non egestas nibh."}
-                    step3={"Step 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium velit in lorem tempus vestibulum non egestas nibh."}
+                    step1={"Step 1: Search for the business projects that match your interests and skillsets."}
+                    step2={"Step 2: Review the project details and click the “apply” button."}
+                    step3={"Step 3: Fill in an application form and wait for approval from the business side."}
                 />
             </div>
             <h1>Business</h1>
@@ -49,9 +49,8 @@ const Home = () => {
                 <HomeSection
                     subtitle={"How to request a project?"}
                     onTryNowClick={onTryNow}
-                    step1={"Step 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium velit in lorem tempus vestibulum non egestas nibh."}
-                    step2={"Step 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium velit in lorem tempus vestibulum non egestas nibh."}
-                    step3={"Step 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium velit in lorem tempus vestibulum non egestas nibh."}
+                    step1={"Step 1: Fill in related documents and information of your project."}
+                    step2={"Step 2: Review your project details and click “Submit” when you are finished."}
                 />
                 <Business/>
             </div>
@@ -60,24 +59,24 @@ const Home = () => {
                 <HomeSection
                     subtitle={"How to find your ideal team?"}
                     onTryNowClick={onTryNow}
-                    step1={"Step 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium velit in lorem tempus vestibulum non egestas nibh."}
-                    step2={"Step 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium velit in lorem tempus vestibulum non egestas nibh."}
-                    step3={"Step 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium velit in lorem tempus vestibulum non egestas nibh."}
+                    step1={"Step 1: Search for student projects that match your business ambition and goal."}
+                    step2={"Step 2: Go through the project details to look for your match."}
+                    step3={"Step 3: Connect with the student team using team cotact below."}
                 />
             </div>
             <h1>Why Us?</h1>
             <div className={"section2-wrapper"}>
                 <div className={"section"}>
                     <WhyUs1/>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <p>Business will get vertification from the academic institutios.</p>
                 </div>
                 <div className={"section"}>
                     <WhyUs2/>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <p>Students can form a team in the platform, which gives them more bargaining power.</p>
                 </div>
                 <div className={"section"}>
                     <WhyUs3/>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <p>It is interactive platform for business and student team.</p>
                 </div>
             </div>
             <h1>Contact Us</h1>
@@ -87,28 +86,28 @@ const Home = () => {
                     title={"Project Manager / Front-End Developer"}
                     name={"Florida Joca"}
                     email={"florida.joca@gmail.com"}
-                    linkedIn={"florida.joca@gmail.com"}
+                    linkedIn={"florida-joca"}
                 />
                 <ContactProfile
                     photo={"https://picsum.photos/200"}
                     title={"Development Lead / Back-End Developer"}
                     name={"Diego Santa Cruz Meono"}
-                    email={"florida.joca@gmail.com"}
-                    linkedIn={"florida.joca@gmail.com"}
+                    email={"diego.santacruz@outlook.com"}
+                    linkedIn={"diegosantacruzm"}
                 />
                 <ContactProfile
                     photo={"https://picsum.photos/200"}
                     title={"Full-Stack Developer"}
                     name={"Rojin Taghadosi"}
-                    email={"florida.joca@gmail.com"}
-                    linkedIn={"florida.joca@gmail.com"}
+                    email={"rojintg@gmail.com"}
+                    linkedIn={"rojin-taghadosi"}
                 />
                 <ContactProfile
                     photo={"https://picsum.photos/200"}
                     title={"Full-Stack Developer"}
                     name={"Winall Lopes"}
-                    email={"florida.joca@gmail.com"}
-                    linkedIn={"florida.joca@gmail.com"}
+                    email={"winalllopes1@gmail.com"}
+                    linkedIn={"win-all-lopes"}
                 />
             </div>
             <div className={"contact-wrapper"}>
@@ -116,29 +115,29 @@ const Home = () => {
                     photo={"https://picsum.photos/200"}
                     title={"Design Lead / UI/UX Designer"}
                     name={"Andrew Yip"}
-                    email={"florida.joca@gmail.com"}
-                    linkedIn={"florida.joca@gmail.com"}
+                    email={"andrewyip30@gmail.com"}
+                    linkedIn={"andrewthyip"}
                 />
                 <ContactProfile
                     photo={"https://picsum.photos/200"}
                     title={"UI/UX Designer"}
                     name={"Jane Choi"}
-                    email={"florida.joca@gmail.com"}
-                    linkedIn={"florida.joca@gmail.com"}
+                    email={"janecanada92@gmail.com"}
+                    linkedIn={"janechoi92"}
                 />
                 <ContactProfile
                     photo={"https://picsum.photos/200"}
                     title={"UI/UX Designer"}
                     name={"Javier Perez"}
                     email={"florida.joca@gmail.com"}
-                    linkedIn={"florida.joca@gmail.com"}
+                    linkedIn={"javier-pr"}
                 />
                 <ContactProfile
                     photo={"https://picsum.photos/200"}
                     title={"UI/UX Designer"}
                     name={"Jasmin Fazelinjad"}
-                    email={"florida.joca@gmail.com"}
-                    linkedIn={"florida.joca@gmail.com"}
+                    email={"yfazeli49@gmail.com"}
+                    linkedIn={"yasfazeli"}
                 />
             </div>
             <div className={"contact-form-wrapper"}>
