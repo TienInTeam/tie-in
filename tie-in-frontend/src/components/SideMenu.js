2d783f7d00a7a3537164df53290391d51e61e34e
rojintag <106949675+rojintag@users.noreply.github.com>
2022-11-28 17:35:41 -0800

    LUNA-211 style: style the side menu

--- tie-in-frontend/src/components/SideMenu.js
+++ tie-in-frontend/src/components/SideMenu.js
@@ -1,15 +1,42 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { getBusinessByEmail, getBusinesses } from '../api/business';
+import { getStudentByEmail, requestStudents } from '../api/student';
 
 function SideMenu() {
   const userType = sessionStorage.getItem("userType");
   const isLoggedIn = sessionStorage.getItem("userLoggedIn");
 
+   const requestBusiness = useQuery(["business"], () => getBusinesses(),
+    {
+      onError: (error) => {
+        alert(error.message);
+      }
+    });
+  const requestStudent = useQuery(["student"], () => requestStudents(),
+    {
+      onError: (error) => {
+        alert(error.message);
+      }
+    });
+  if (requestBusiness.isLoading) {
+    return <span>Loading...</span>
+  }
+  if (requestStudent.isLoading) {
+    return <span>Loading...</span>
+  }
+
+  console.log(requestStudent.data);
   if (isLoggedIn === "true") {
     if (userType === "business") {
       return (
         <div className="side-menu">
           <>
+          <div className="image-wrapper">
+            <img src="https://firebasestorage.googleapis.com/v0/b/tie-in-b8590.appspot.com/o/pexels-cottonbro-studio-6209564.jpg?alt=media&token=69187a6b-7c6b-470c-9c36-b45eeb9e3d83" alt="business" />
+            <h3>Business</h3>
+          </div>
             <nav>
               <ul>
                 <li>
@@ -34,6 +61,10 @@ function SideMenu() {
       return (
         <div className="side-menu">
           <>
+          <div className="image-wrapper">
+            <img src="https://firebasestorage.googleapis.com/v0/b/tie-in-b8590.appspot.com/o/pexels-cottonbro-studio-6209564.jpg?alt=media&token=69187a6b-7c6b-470c-9c36-b45eeb9e3d83" alt="business" />
+            <h3>Student</h3>
+          </div>
             <nav>
               <ul>
                 <li>
