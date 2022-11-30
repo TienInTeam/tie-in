8f088a7f8c971072dc57b3054479057d38b0f059
FloridaLangara <fjoca00@mylangara.ca>
2022-11-30 11:38:58 -0800

    fix: Fix the images for teams and styling

--- tie-in-frontend/src/components/RequestStatusCard.js
+++ tie-in-frontend/src/components/RequestStatusCard.js
@@ -1,31 +1,30 @@
 // Student Dashboard Application Status
 
+import {useQuery} from "@tanstack/react-query";
 import React from 'react';
+import {requestTeamsByTeamId} from "../api/teams";
 
-function RequestStatusCard({ application }) {
-  const { team, team_images, name, created_at, status, business } = application;
-  return (
-    <div className="request-status-card">
-      <p>{business.business_name}</p>
-      <div className="team-wrapper team-wrapper-content">
-        <h3>{team.team_name}</h3>
-        <div className="team-images-wrapper" id={`team${team.team_id}`}>
-          {/*{*/}
-          {/*  team_images?.map((image, index) => {*/}
-          {/*    if (index <= 2) {*/}
-          {/*      return <img key={index} src={image} alt="student" />*/}
-          {/*    }*/}
-          {/*    else if (index === team_images.length - 1) {*/}
-          {/*      return <p key={index}>+<span >{index - 2}</span></p>*/}
-          {/*    }*/}
-          {/*  }*/}
-          {/*  )}*/}
+function RequestStatusCard({application}) {
+    const {team, created_at, status, business} = application;
+
+    const requestTeam = useQuery(["teams"], () => requestTeamsByTeamId(team.team_id))
+    return (
+        <div className="request-status-card">
+            <p>{business.business_name}</p>
+            <div className="team-wrapper team-wrapper-content">
+                <h3>{team.team_name}</h3>
+                <div className="team-images-wrapper" id={`${team.team_id}`}>
+                    {
+                        requestTeam.data?.members?.map((member, index) => {
+                            return <img key={index} src={member.photo_url} alt="student"/>
+                        })
+                    }
+                </div>
+            </div>
+            <p>{new Date(created_at).toDateString()}</p>
+            <p>{status}</p>
         </div>
-      </div>
-      <p>{new Date(created_at).toDateString()}</p>
-      <p>{status}</p>
-    </div>
-  );
+    );
 }
 
 export default RequestStatusCard;
