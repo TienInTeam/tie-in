43f686e2458309575335d0de41d1b2b94deef825
FloridaLangara <fjoca00@mylangara.ca>
2022-11-29 18:16:58 -0800

    fix: Fix approving project on company site

--- tie-in-frontend/src/components/TeamApplication.js
+++ tie-in-frontend/src/components/TeamApplication.js
@@ -1,43 +1,26 @@
-import {useQuery} from "@tanstack/react-query";
-import { requestTeamsByTeamId} from "../api/teams";
 import TeamsBox from "../components/TeamsBox";
 import Button from "./Button";
 
-function TeamApplication({name, logo_url, teamId, status,  onClose, onApprove, businessProjectStatus}) {
-    const businessId = sessionStorage.getItem("userMongoId");
-
-    const requestTeam = useQuery(["teams"], () => requestTeamsByTeamId(teamId), {
-        onError: (error) => {
-            alert(error.message);
-        }
-    });
-
-    // const renderMembers = () => {
-    //     return requestTeam.data.members.map((member) => {
-    //         return
-    //     } )
-    // }
-
+function TeamApplication({name, logo_url, team, applicationStatus, onClose, onApprove}) {
     return (
         <div className="team-application">
             <div className="title-wrapper">
                 <div className="logo-wrapper">
-                     <h2>{name}</h2>
+                    <h2>{name}</h2>
                 </div>
                 <Button label={"Close Project"} variant={"secondary"} onClick={onClose}/>
             </div>
             <div>
-                  <TeamsBox
-                      name={requestTeam.data?.name}
-                    // members={team.members}
-                    status={status}
-                      businessProjectStatus={businessProjectStatus}
-                    teamId={teamId}
+                <TeamsBox
+                    name={team.team_name}
+                    members={team.members}
+                    applicationStatus={applicationStatus}
+                    teamId={team.team_id}
                     onApprove={onApprove}
-                  />
+                />
             </div>
         </div>
     )
 }
 
-export default TeamApplication
+export default TeamApplication;
