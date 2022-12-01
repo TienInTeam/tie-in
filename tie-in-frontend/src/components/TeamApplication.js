e1a6a0884c114eb43e6ed83996abfd2615ef88ce
rojintag <106949675+rojintag@users.noreply.github.com>
2022-12-01 14:17:53 -0800

    LUNA-266 feat: Add logo of company for each request

--- tie-in-frontend/src/components/TeamApplication.js
+++ tie-in-frontend/src/components/TeamApplication.js
@@ -1,11 +1,12 @@
 import TeamsBox from "../components/TeamsBox";
 import Button from "./Button";
 
-function TeamApplication({name, logo_url, team, applicationStatus, onClose, onApprove}) {
+function TeamApplication({name, logo, team, applicationStatus, onClose, onApprove}) {
     return (
         <div className="team-application">
             <div className="title-wrapper">
                 <div className="logo-wrapper">
+                    <img src={logo} alt="logo" />
                     <h2>{name}</h2>
                 </div>
                 <Button label={"Close Project"} variant={"secondary"} onClick={onClose}/>
