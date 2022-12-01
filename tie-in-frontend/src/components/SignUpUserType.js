7350f69acb7c5d7f4fa8a6c3b3c8af0722138f52
Diego Santa Cruz <dsantacruzmeoo00@mylangara.ca>
2022-11-30 17:40:30 -0800

    style: home page styling - desktop view

--- tie-in-frontend/src/components/SignUpUserType.js
+++ tie-in-frontend/src/components/SignUpUserType.js
@@ -1,6 +1,6 @@
 import {Link} from "react-router-dom";
-import { ReactComponent as StudentType } from '../assets/icons/home/student-type.svg';
-import { ReactComponent as BusinessType } from '../assets/icons/home/business-type.svg';
+import { ReactComponent as StudentType } from '../assets/icons/home/student_image_1.svg';
+import { ReactComponent as BusinessType } from '../assets/icons/home/business_image_1.svg';
 
 
 const SignUpUserType = () => {
