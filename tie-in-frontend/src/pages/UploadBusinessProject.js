1b8d5bdafe5c2e34e25c8047d35b77dfffcf2fc2
FloridaLangara <fjoca00@mylangara.ca>
2022-12-02 11:32:45 -0800

    chore: fix minor stylings

--- tie-in-frontend/src/pages/UploadBusinessProject.js
+++ tie-in-frontend/src/pages/UploadBusinessProject.js
@@ -90,7 +90,7 @@ function UploadBusinessProject() {
 
     const onSubmit = () => {
         if (validateInput()) {
-            // saveProject.mutate();
+            saveProject.mutate();
             setSuccess(true)
         }
     }
